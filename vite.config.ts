872a816de20c4a52ad02babeea57e9e346e0e326
andrewmatti12 <144314970+andrewmatti12@users.noreply.github.com>
2026-06-30 18:10:59 +0000

    Set up GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const repoName = 'amamamamamamama'
-
 export default defineConfig({
   plugins: [react()],
-  base: process.env.GITLAB_PAGES ? `/${repoName}/` : '/',
+  base: '/amamamamamamama/',
 })
